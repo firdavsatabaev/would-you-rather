f1facaba5903d8e44c7890dd80cf726b95cd91a4
8Firdavs8 <atabfi01@luther.edu>
2022-02-01 21:40:39 -0600

    Updates

---
+++ precache-manifest.90739dddea528af00735cd41a2f541f3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bfbb15dcd999ae4f12d49fd5f14a572",
+    "url": "/would-you-rather/index.html"
+  },
+  {
+    "revision": "bf4a99ea71ca956a70ab",
+    "url": "/would-you-rather/static/css/main.9ad156cc.chunk.css"
+  },
+  {
+    "revision": "b961464c82cb3b5c2685",
+    "url": "/would-you-rather/static/js/2.f9e901a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/would-you-rather/static/js/2.f9e901a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf4a99ea71ca956a70ab",
+    "url": "/would-you-rather/static/js/main.222a4daa.chunk.js"
+  },
+  {
+    "revision": "4e275b564a8bb03f6c59",
+    "url": "/would-you-rather/static/js/runtime-main.fb0fae1a.js"
+  },
+  {
+    "revision": "e71d806c5ece520ae3ac0e61d32f065d",
+    "url": "/would-you-rather/static/media/404.e71d806c.svg"
+  },
+  {
+    "revision": "a442529dfb6b0fc3861a9eef7848b9aa",
+    "url": "/would-you-rather/static/media/avatar1.a442529d.svg"
+  },
+  {
+    "revision": "7f3eeb9d5e26c174f3d3c17b0e2f34b2",
+    "url": "/would-you-rather/static/media/avatar2.7f3eeb9d.svg"
+  },
+  {
+    "revision": "79c5652d8cdc5d15631d0f18cce5f9b1",
+    "url": "/would-you-rather/static/media/avatar3.79c5652d.svg"
+  },
+  {
+    "revision": "93b4498448ca853202e6d056f4a1cd52",
+    "url": "/would-you-rather/static/media/avatar4.93b44984.svg"
+  },
+  {
+    "revision": "564fbbb97957f42510a18702e9147be4",
+    "url": "/would-you-rather/static/media/avatar5.564fbbb9.svg"
+  }
+]);
